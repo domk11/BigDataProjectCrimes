a050b25f0ca529019b8c89e3902a5c4128510c14
Marco Lagalla <marcolagalla@gmail.com>
2020-09-13 11:15:41 +0200

    fix

--- filter_original_dataset.py
+++ filter_original_dataset.py
@@ -4,9 +4,9 @@ from src.database import connect_db, Database
 if __name__ == '__main__':
     db = Database(connect_db().get_database())
 
-    # filtered = db.filter_data()
-    # for data in filtered:
-    #     db.put_filtered_data(data)
+    filtered = db.filter_data()
+    for data in filtered:
+        db.put_filtered_data(data)
 
     filtered = db.filter_census_data()
     for data in filtered:
