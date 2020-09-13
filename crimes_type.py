2efcea35dcd82c12bed88b70df375b8c8f532594
Marco Lagalla <marcolagalla@gmail.com>
2020-09-13 10:56:33 +0200

    ml

--- crimes_type.py
+++ crimes_type.py
@@ -14,37 +14,37 @@ def main():
 
         ny = SparkNYPD(nypd_df)
 
-        ny.crimes_trend(img_out=output_base + 'crimes_trend.png',
-                        csv_out=output_base + 'crimes_trend.csv')
-
-        ny.crimes_top(img_out=output_base + 'crimes_top.png',
-                      csv_out=output_base + 'crimes_top.csv')
-
-        ny.crimes_severity(img_out=output_base + 'crimes_severity.png',
-                           csv_out=output_base + 'crimes_severity.csv')
-
-        # TODO: fix error in plot
-        # ny.crimes_severity_by_district(img_out=output_base + 'crimes_severity_by_district.png',
-        #                                csv_out=output_base + 'crimes_severity_by_district.csv')
-
-        ny.crimes_day_night(img_out=output_base + 'crimes_daynight.png',
-                            csv_out=output_base + 'crimes_daynight.csv')
-
-        ny.crimes_race(img_out=output_base + 'crimes_race.png',
-                       csv_out=output_base + 'crimes_race.csv')
-
-        ny.crimes_cross_districts(img_out=output_base + 'crimes_cross_districts.png',
-                                  csv_out=output_base + 'crimes_cross_districts.csv')
-
-        ny.crimes_cross_districts_race(img_out=output_base + 'crimes_districts_race.png',
-                                       csv_out=output_base + 'crimes_districts_race.csv')
+        # ny.crimes_trend(img_out=output_base + 'crimes_trend.png',
+        #                 csv_out=output_base + 'crimes_trend.csv')
+        #
+        # ny.crimes_top(img_out=output_base + 'crimes_top.png',
+        #               csv_out=output_base + 'crimes_top.csv')
+        #
+        # ny.crimes_severity(img_out=output_base + 'crimes_severity.png',
+        #                    csv_out=output_base + 'crimes_severity.csv')
+        #
+        # # TODO: fix error in plot
+        # # ny.crimes_severity_by_district(img_out=output_base + 'crimes_severity_by_district.png',
+        # #                                csv_out=output_base + 'crimes_severity_by_district.csv')
+        #
+        # ny.crimes_day_night(img_out=output_base + 'crimes_daynight.png',
+        #                     csv_out=output_base + 'crimes_daynight.csv')
+        #
+        # ny.crimes_race(img_out=output_base + 'crimes_race.png',
+        #                csv_out=output_base + 'crimes_race.csv')
+        #
+        # ny.crimes_cross_districts(img_out=output_base + 'crimes_cross_districts.png',
+        #                           csv_out=output_base + 'crimes_cross_districts.csv')
+        #
+        # ny.crimes_cross_districts_race(img_out=output_base + 'crimes_districts_race.png',
+        #                                csv_out=output_base + 'crimes_districts_race.csv')
 
         print("Done")
 
     except Exception as e:
         print(e)
     finally:
-        sc.stop()
+        spark.sparkContext.stop()
 
 
 if __name__ == '__main__':
